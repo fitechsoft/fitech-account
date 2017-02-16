ca810d3490c7c1e64d30b5204d1fec91300470fc
Chun Cao <chun@114-212-85-245.nju.edu.cn>
2017-02-16 16:29:11 +0800

    project created

---
+++ src/main/java/com/fitech/model/base/FTEntity.java
@@ -0,0 +1,7 @@
+package com.fitech.model.base;
+
+/**
+ * Created by chun on 2017/2/16.
+ */
+public class FTEntity {
+}
---
+++ src/main/java/com/fitech/model/base/FTSubject.java
@@ -0,0 +1,8 @@
+package com.fitech.model.base;
+
+/**
+ * Created by chun on 2017/2/16.
+ */
+public class FTSubject extends FTEntity{
+    
+}
