a04da3480ac95e2e140d7c84e39fbbda4ef5a35c
Chun Cao <chun@114-212-85-245.nju.edu.cn>
2017-02-18 18:03:16 +0800

    add pom parent

--- src/main/java/com/fitech/base/model/FTEntity.java
+++ src/main/java/com/fitech/base/model/FTEntity.java
@@ -1,7 +1,29 @@
 package com.fitech.base.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  * Created by chun on 2017/2/16.
  */
+@MappedSuperclass
 public class FTEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer id;
+
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public boolean isNew() {
+        return (this.id == null);
+    }
 }
